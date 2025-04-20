989951d8d807f45b4c45351c56f4cc76085685cf
Phuc114 <128774565+Phuc114@users.noreply.github.com>
2025-04-20 23:45:21 +0700

    Add files via upload

---
+++ Database/SourceCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp.Forms; // hoặc nơi chứa NotificationForm
+
+
+namespace WindowsFormsApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new NotificationForm());// sửa tạm để test
+            Application.Run(new LoginForm());
+        }
+    }
+}
