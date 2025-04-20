84e330ae445ce344acda3fcbfb6a2b984e76a806
Phuc114 <128774565+Phuc114@users.noreply.github.com>
2025-04-20 23:43:15 +0700

    Add files via upload

---
+++ Database/SourceCode/Data/DbConnectionHelper.cs
@@ -0,0 +1,13 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace WindowsFormsApp.Data
+{
+    public static class DbConnectionHelper
+    {
+        public static OracleConnection GetConnection(string username, string password)
+        {
+            string connStr = $"DATA SOURCE=localhost:1521/xe;USER ID={username};PASSWORD={password};";
+            return new OracleConnection(connStr);
+        }
+    }
+}
---
+++ Database/SourceCode/Data/UserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace WindowsFormsApp.Data
+{
+    public static class UserSession
+    {
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+        public static string Role { get; set; }
+    }
+}
