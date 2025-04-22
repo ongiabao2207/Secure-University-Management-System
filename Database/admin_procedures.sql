529292010e6ffcf154b9457753b767de61c69015
ongiabao2207 <ongiabao36@gmail.com>
2025-04-22 22:49:07 +0700

    Add procs to create role, drop role, update role (password)

--- Database/admin_procedures.sql
+++ Database/admin_procedures.sql
@@ -47,7 +47,7 @@ END;
 --declare
 --    res number;
 --begin
---   SP_CheckUserExists('NV001', res);
+--   SP_Check_User_Exists('NV001', res);
 --   dbms_output.put_line(res);
 --end;
 --/
@@ -178,3 +178,114 @@ END;
 /
 --exec SP_Lock_User('SV4001');
 --exec SP_Unlock_User('SV4001');
+
+
+-- Proc kiểm tra role đã tồn tại hay chưa, trả về 0 nếu không tồn tại, trả về 1 nếu đã tồn tại
+CREATE OR REPLACE PROCEDURE SP_Check_Role_Exists (
+    role_ in char,
+    result_ out number
+)
+AS
+    cnt int;
+BEGIN
+    SELECT count(*) into cnt FROM DBA_ROLES WHERE role = role_;
+    if cnt > 0 then
+        result_ := 1;
+    else
+        result_ := 0;
+    end if;
+END;
+/
+declare
+    res number;
+begin
+   SP_Check_Role_Exists('SV', res);
+   dbms_output.put_line(res);
+end;
+/
+
+
+-- Proc tạo mới role
+CREATE OR REPLACE PROCEDURE SP_Create_Role (
+    role_ in char,
+    password_ in char
+)
+AS
+    is_exists number;
+    str_SQL VARCHAR2(1000);
+BEGIN
+    -- Kiểm tra role đã tồn tại hay chưa
+    SP_Check_Role_Exists(role_, is_exists);
+    if is_exists = 0 then -- nếu chưa tồn tại
+        begin
+            if password_ is NULL then -- để trống password
+                str_SQL := 'CREATE ROLE "' || role_ || '"';
+            else
+                str_SQL := 'CREATE ROLE "' || role_ || '" IDENTIFIED BY "' || password_ || '"';
+            end if;
+            EXECUTE IMMEDIATE str_SQL;
+        end;
+    else -- nếu đã tồn tại
+        -- Ném lỗi
+        RAISE_APPLICATION_ERROR(-20007, 'Role "' || role_ || '" has already existed!');
+    end if;
+END;
+/
+--exec sp_create_role('test_role', '111');
+
+-- Proc xoá role
+CREATE OR REPLACE PROCEDURE SP_Drop_Role (
+    role_ IN VARCHAR2
+)
+AS
+    is_exists NUMBER;
+    str_SQL VARCHAR2(1000);
+BEGIN
+    -- Kiểm tra role có tồn tại không
+    SP_Check_Role_Exists(role_, is_exists);
+
+    IF is_exists = 1 THEN -- nếu đã tồn tại
+        str_SQL := 'DROP ROLE "' || role_ || '"';
+        EXECUTE IMMEDIATE str_SQL;
+    ELSE -- nếu chưa tồn tại
+        RAISE_APPLICATION_ERROR(-20008, 'Role  "' || role_ || '" does not exist!');
+    END IF;
+END;
+/
+--exec SP_Drop_Role('test_role');
+
+
+-- Proc chỉnh sửa mật khẩu cho role
+CREATE OR REPLACE PROCEDURE SP_Change_Role_Password (
+    role_ in char,
+    new_pass in char,
+    confirm_new_pass in char
+)
+AS
+    is_exists NUMBER;
+    str_SQL VARCHAR2(1000);
+BEGIN
+    -- Kiểm tra role có tồn tại không
+    SP_Check_Role_Exists(role_, is_exists);
+    
+    if is_exists = 0 then -- nếu không tồn tại
+        RAISE_APPLICATION_ERROR(-20009, 'Role "' || role_ || '" does not exist!');
+    else
+        begin
+            if new_pass != confirm_new_pass then
+                RAISE_APPLICATION_ERROR(-20010, 'Different password!');
+            else
+                if new_pass is NULL then
+                    str_SQL := 'ALTER ROLE "' || role_ || '" NOT IDENTIFIED';
+                else
+                    str_SQL := 'ALTER ROLE "' || role_ || '" IDENTIFIED BY "' || new_pass || '"';
+                end if;
+                EXECUTE IMMEDIATE str_SQL;
+            end if;
+        end;
+    end if;
+END;
+/
+--exec SP_CHANGE_ROLE_PASSWORD('test_role', '123', '123');
+--SELECT ROLE, ROLE_ID, PASSWORD_REQUIRED, AUTHENTICATION_TYPE FROM DBA_ROLES WHERE ROLE = 'test_role'
+--ORDER BY ROLE_ID;
