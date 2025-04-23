80ad909f27a87c7e04d821c44f5be3906f1e1215
Phuc114 <128774565+Phuc114@users.noreply.github.com>
2025-04-23 13:53:37 +0700

    Create TrgdvForm.cs

---
+++ Database/SourceCode/Data/Roles/TrgdvForm.cs
@@ -0,0 +1,13 @@
+using System.Windows.Forms;
+using WindowsFormsApp.Forms;
+
+namespace WindowsFormsApp.Forms
+{
+    public class TrgdvForm : Form
+    {
+        public TrgdvForm()
+        {
+            this.InitUI("Trang Trưởng đơn vị");
+        }
+    }
+}
