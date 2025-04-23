dbb570a62170329f3959252a881920553c1b2c13
Phuc114 <128774565+Phuc114@users.noreply.github.com>
2025-04-23 13:51:53 +0700

    Add files via upload

---
+++ Database/SourceCode/Forms/CommonUserForm.cs
@@ -0,0 +1,42 @@
+﻿// CommonUserForm.cs
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WindowsFormsApp.Data;
+
+namespace WindowsFormsApp.Forms
+{
+    public static class CommonUserForm
+    {
+        public static void InitUI(this Form form, string title)
+        {
+            form.Text = title;
+            form.Size = new Size(600, 400);
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.BackColor = Color.White;
+
+            Label lbl = new Label()
+            {
+                Text = $"Xin chào {UserSession.Hoten} - Vai trò: {UserSession.Role}",
+                Font = new Font("Segoe UI", 14),
+                AutoSize = true,
+                Location = new Point(50, 80)
+            };
+            form.Controls.Add(lbl);
+
+            Button btnXemThongBao = new Button()
+            {
+                Text = "📢 Xem thông báo",
+                Size = new Size(180, 40),
+                Location = new Point(50, 140),
+                BackColor = Color.LightSteelBlue
+            };
+            btnXemThongBao.Click += (s, e) =>
+            {
+                NotificationForm frm = new NotificationForm();
+                frm.ShowDialog();
+            };
+            form.Controls.Add(btnXemThongBao);
+        }
+    }
+}
---
+++ Database/SourceCode/Forms/StudentForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WindowsFormsApp.Data;
+
+namespace WindowsFormsApp.Forms
+{
+    public class StudentForm : Form
+    {
+        public StudentForm()
+        {
+            this.Text = "Trang Sinh viên";
+            this.Size = new Size(600, 400);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.White;
+
+            Label lbl = new Label()
+            {
+                Text = $"Xin chào {UserSession.Hoten} - Mã SV: {UserSession.Username}",
+                Font = new Font("Segoe UI", 14),
+                AutoSize = true,
+                Location = new Point(50, 80)
+            };
+            this.Controls.Add(lbl);
+
+            Button btnXemThongBao = new Button()
+            {
+                Text = "📢 Xem thông báo",
+                Size = new Size(180, 40),
+                Location = new Point(50, 140),
+                BackColor = Color.LightSteelBlue
+            };
+            btnXemThongBao.Click += (s, e) =>
+            {
+                NotificationForm frm = new NotificationForm();
+                frm.ShowDialog();
+            };
+            this.Controls.Add(btnXemThongBao);
+        }
+    }
+}
